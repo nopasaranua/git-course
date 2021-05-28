082bda5199657b71d0837ee1aafb8b30473bf5c3
Mihail  Goncharov <mishagoncharov1991@gmail.com>
2021-05-28 19:31:40 +0300

    add style css and click h1

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-document.body.innerHTML = '<h1>Hello from Java script</h1>'
-console.log('test !!!!!!')
+document.body.innerHTML = '<h1>Hello from Java script</h1>';
+console.log('test !!!!!!');
+h1 = document.querySelector('h1');
+h1.addEventListener('click', function (){
+    this.style.color = 'red';
+})
